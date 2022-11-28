4b35de84aa29f829bb17009bb90dbbf170e754a6
Shajwan <shajwannamiqsalh@gmail.com>
2022-11-28 21:14:14 +0300

    adding structure

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import {
+   Billing,
+   Business,
+   CardDeal,
+   Clients,
+   CTA,
+   Footer,
+   Navbar,
+   Stats,
+   Testimonials,
+   Hero,
+ } from "./components";
+ 
+ 
+function App() {
+  return (
+    <>
+      
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
