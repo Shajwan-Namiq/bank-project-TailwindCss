2e5d5ce860246ba871d413815c31b63b1975b733
Shajwan <shajwannamiqsalh@gmail.com>
2022-11-30 22:51:26 +0300

    adding header of testimonial

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -3,13 +3,28 @@ import React from 'react'
 const Testimonials = () => {
   return (
     <>
-    
-    
-    
-    
-    
+      <header class="  ">
+        <div class="container px-6 py-16 mx-auto">
+          <div class="items-center lg:flex">
+            <div class="w-full lg:w-1/2">
+              <div class="lg:max-w-lg">
+                <h1 class="text-3xl font-semibold text-color lg:text-6xl">
+                  What people are saying about us{" "}
+                </h1>
+              </div>
+            </div>
+
+            <div class="flex lg:mr-10 items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
+              <p class="lg:max-w-md mt-6 text-lg text-section">
+                Everything you need to accept card payments and grow your
+                business anywhere on the planet.
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
     </>
-  )
+  );
 }
 
 export default Testimonials
